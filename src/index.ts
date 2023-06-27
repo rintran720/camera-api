60dab4d3f0f60373e4e9071794941eb21d59994e
johntranviact <john.tran@viact.ai>
2023-06-27 12:20:32 +0700

    feat: export func

--- src/index.ts
+++ src/index.ts
@@ -4,3 +4,10 @@ global.__config = loadConfig();
 export function greet(name: string): string {
   return `Hello, ${name}!`;
 }
+
+export * from "./utils/net";
+export * from "./services/Camera";
+export * from "./services/DahuaCamera";
+export * from "./services/HikvisionCamera";
+export * from "./services/ScanDevice";
+export * from "./types/camera";
